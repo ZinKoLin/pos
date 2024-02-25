87e1ba92cb062ecf2724513ca0da55a3e60facfa
ZinKoLin <zinkolin113@gmail.com>
2024-02-25 21:36:55 +0700

    admin template setup

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,7 +8,7 @@
         <!-- Email Address -->
         <div>
             <x-input-label for="login" :value="__('Email/Name/Phone')" />
-            <x-text-input id="login" class="block mt-1 w-full" type="login" name="login" :value="old('login')" required autofocus autocomplete="username" />
+            <x-text-input id="login" class="block mt-1 w-full" type="text" name="login" :value="old('login')" required autofocus autocomplete="username" />
         </div>
 
         <!-- Password -->
